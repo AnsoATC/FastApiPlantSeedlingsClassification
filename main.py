4f0d992b0ffc8a97784da1f29cad5fa135543b61
anselmeatchogou <anselmeatchogou@gmail.com>
2024-01-02 09:28:30 +0300

    Correction of the code by adding appropiate librairy version

--- main.py
+++ main.py
@@ -6,9 +6,11 @@ from io import BytesIO
 from PIL import Image
 import tensorflow as tf
 import cv2
+import random
 
 app = FastAPI()
 
+# Uncomment the CORS middleware if your API is being accessed from a different domain (e.g., a frontend application)
 # origins = [
 #     "http://localhost",
 #     "http://localhost:3000",
@@ -22,7 +24,7 @@ app = FastAPI()
 # )
 
 # Load your trained model
-MODEL = tf.keras.models.load_model('saved_models/best_model_3.keras')  # Update with your model path
+MODEL = tf.keras.models.load_model('saved_models/plant-seedling-2.h5')  # Update with your model path
 
 # Define class names as per your model
 CLASS_NAMES = ['Black-grass', 'Charlock', 'Cleavers', 'Common Chickweed',
@@ -35,36 +37,33 @@ async def ping():
     return "Hello, I am alive"
 
 def preprocess_image(image: Image.Image) -> np.ndarray:
-    # Convert PIL Image to NumPy array
+    # Convert PIL Image to NumPy array and preprocess as per your model's requirements
     img = np.array(image)
-
-    # Resize, convert to HSV, apply mask, etc., as per your model's training
-    img = cv2.resize(img, (128, 128))
-    blurr = cv2.GaussianBlur(img, (5, 5), 0)
-    hsv = cv2.cvtColor(blurr, cv2.COLOR_BGR2HSV)
-    # ... continue with your specific preprocessing
-
-    # Normalize the image
-    img = img.astype('float32') / 255.0
-
+    img = cv2.resize(img, (128, 128))  # Resize to match model's expected input size
+    # Add other preprocessing steps here if needed
+    img = img.astype('float32') / 255.0  # Normalize the image
     return img
 
 @app.post("/predict")
-async def predict(
-    file: UploadFile = File(...)
-):
+async def predict(file: UploadFile = File(...)):
+    # Read the uploaded image
     image = Image.open(BytesIO(await file.read()))
     processed_image = preprocess_image(image)
-    img_batch = np.expand_dims(processed_image, 0)
+    img_batch = np.expand_dims(processed_image, 0)  # Create a batch
 
+    # Make a prediction
     predictions = MODEL.predict(img_batch)
-
     predicted_class = CLASS_NAMES[np.argmax(predictions[0])]
     confidence = np.max(predictions[0])
+
+    # Generate a random confidence value from the predefined list
+    random_confidence_values = [0.77, 0.89, 0.91, 0.94, 0.99, 1]
+    #confidence = random.choice(random_confidence_values)
+
     return {
         'class': predicted_class,
-        'confidence': float(confidence)
+        'confidence': float(confidence) #confidence #float(confidence)
     }
 
-# if __name__ == "__main__":
-#     uvicorn.run(app, host='localhost', port=8000)
+if __name__ == "__main__":
+    uvicorn.run(app, host='localhost', port=8000)  # Run the app
